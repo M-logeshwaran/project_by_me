2f001378359058ffedff68f824766c8dc0594a0b
M_LOKI_458 <mg.logesh458@gmail.com>
2025-07-28 21:32:39 +0530

    Update to py_hand_cricket_(op).py

--- py_hand_cricket_(op).py
+++ py_hand_cricket_(op).py
@@ -1,156 +1,101 @@
 Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
 Enter "help" below or click "Help" above for more information.
 >>> 
-= RESTART: C:/Users/Admin/Downloads/all_program_app/language scripts/python_my_scripts/py_project _odd_or_even.py
+= RESTART: C:\Users\Admin\Downloads\all_program_app\language scripts\python_my_scripts\hand_cricket.py
 
-	 Enter YES to START GAME or NO to QUIT :YES
+		 Enter YES to START GAME or NO to QUIT :YES
 
-	     >>> USE INT TYPE and BLOCK LETTERS ONLY !
-
+		 >>> USE INT TYPE and BLOCK LETTERS ONLY !
+   
 ---------------------------------------------------------------------------------------
 
 		 >>> ODD or EVEN GAME !
 
 		* Enter your choice [ EVEN or ODD ] : ODD
 
-		* my choice is : EVEN
-
-		* Enter your NUM [ INT only ]: 3
-
-		* my choice is : 6
-
-		* TOTAL NUM : 9
-
----------------------------------------------------------------------------------------
-
-		 >>> YOU WON THE TOSS :
-
-		* Enter your choice [ BAT OR BOWL ] : BAT
-
-		* Enter your NUM SCORE [ 0 TO 6 ] for BAT : 4
-
-		* MY NUM SCORE [ 0 TO 6 ] for BOWL :  4
-
-		 >>> YOUR SCORE : 0
-
-		 >>> MY TARGET : 1
-
-		* Enter your NUM SCORE [ 0 TO 6 ] for BOWL : 6
-
-		* MY NUM SCORE [ 0 TO 6 ] for BAT :  1
-
-		 >>> MY SCORE : 1
-
----------------------------------------------------------------------------------------
-
-		 >>> I WON THE GAME !
-
-
----------------------------------------------------------------------------------------
-
-	 Enter YES to START NEW GAME or NO to QUIT :YES
-
-		 >>> ODD or EVEN GAME !
-
-		* Enter your choice [ EVEN or ODD ] : EVEN
-
-		* my choice is : ODD
-
-		* Enter your NUM [ INT only ]: 4
-
-		* my choice is : 5
-
-		* TOTAL NUM : 9
-
----------------------------------------------------------------------------------------
-
-		 >>> I WON THE TOSS :
-
-		* MY choice [ BAT OR BOWL ] :  BOWL
-
-		* Enter your NUM SCORE [ 0 TO 6 ] for BAT : 3
-
-		* MY NUM SCORE [ 0 TO 6 ] for BOWL :  2
-
-		* Enter your NUM SCORE [ 0 TO 6 ] for BAT : 2
-
-		* MY NUM SCORE [ 0 TO 6 ] for BOWL :  2
-
-		 >>> YOUR SCORE : 3
-
-		 >>> MY TARGET : 4
-
-		* Enter your NUM SCORE [ 0 TO 6 ] for BOWL : 6
-
-		* MY NUM SCORE [ 0 TO 6 ] for BAT :  0
-
-		* Enter your NUM SCORE [ 0 TO 6 ] for BOWL : 1
-
-		* MY NUM SCORE [ 0 TO 6 ] for BAT :  5
-
-		 >>> MY SCORE : 5
-
----------------------------------------------------------------------------------------
-
-		 >>> I WON THE GAME !
-
-
----------------------------------------------------------------------------------------
-
-	 Enter YES to START NEW GAME or NO to QUIT :YES
-
-		 >>> ODD or EVEN GAME !
-
-		* Enter your choice [ EVEN or ODD ] : ODD
-
-		* my choice is : EVEN
-
-		* Enter your NUM [ INT only ]: 2
-
-		* my choice is : 5
-
-		* TOTAL NUM : 7
-
----------------------------------------------------------------------------------------
-
-		 >>> YOU WON THE TOSS :
-
-		* Enter your choice [ BAT OR BOWL ] : BAT
-
-		* Enter your NUM SCORE [ 0 TO 6 ] for BAT : 6
-
-		* MY NUM SCORE [ 0 TO 6 ] for BOWL :  2
-
-		* Enter your NUM SCORE [ 0 TO 6 ] for BAT : 6
-
-		* MY NUM SCORE [ 0 TO 6 ] for BOWL :  6
-
-		 >>> YOUR SCORE : 6
-
-		 >>> MY TARGET : 7
-
-		* Enter your NUM SCORE [ 0 TO 6 ] for BOWL : 2
-
-		* MY NUM SCORE [ 0 TO 6 ] for BAT :  2
-
-		 >>> MY SCORE : 0
-
----------------------------------------------------------------------------------------
-
-		 >>> YOU WON THE GAME !
-
----------------------------------------------------------------------------------------
-
-	 Enter YES to START NEW GAME or NO to QUIT :YES
-
-		 >>> ODD or EVEN GAME !
-
-		* Enter your choice [ EVEN or ODD ] : EVEN
-
-		* my choice is : EVEN
+		* COMP choice is : ODD
 
 		 >>> SAME CHOICE GIVEN !
 
 ---------------------------------------------------------------------------------------
 
+		 >>> ODD or EVEN GAME !
+
+		* Enter your choice [ EVEN or ODD ] : ODD
+
+		* COMP choice is : ODD
+
+		 >>> SAME CHOICE GIVEN !
+
+---------------------------------------------------------------------------------------
+
+		 >>> ODD or EVEN GAME !
+
+		* Enter your choice [ EVEN or ODD ] : ODD
+
+		* COMP choice is : EVEN
+
+		* Enter your NUM [ INT only ]: 3
+
+		* COMP choice is : 0
+
+		* TOTAL NUM : 3 (ODD)
+
+---------------------------------------------------------------------------------------
+
+		 >>> YOU WON THE TOSS :
+
+		* Enter your choice [ BAT OR BOWL ] : BAT
+
+		* Enter your NUM SCORE [ 0 TO 6 ] for BAT : 3
+
+		* COMP NUM SCORE [ 0 TO 6 ] for BOWL :  3
+
+---------------------------------------------------------------------------------------
+
+		 >>> YOUR TOTAL SCORE : 0
+
+		 >>> COMP TARGET : 1
+
+---------------------------------------------------------------------------------------
+
+		 SIDE CHANGE !
+
+---------------------------------------------------------------------------------------
+
+		* Enter your NUM SCORE [ 0 TO 6 ] for BOWL : 4
+
+		* COMP NUM SCORE [ 0 TO 6 ] for BAT :  0
+
+---------------------------------------------------------------------------------------
+
+		 >>> COMP CURRENT SCORE : 0
+
+		 >>> BALLS : 1
+
+		* Enter your NUM SCORE [ 0 TO 6 ] for BOWL : 5
+
+		* COMP NUM SCORE [ 0 TO 6 ] for BAT :  1
+
+---------------------------------------------------------------------------------------
+
+		 >>> COMP CURRENT SCORE : 1
+
+		 >>> BALLS : 2
+
+---------------------------------------------------------------------------------------
+
+		 >>> COMP TOTAL SCORE : 1
+
+		 >>> YOUR TOTAL SCORE : 0
+
+---------------------------------------------------------------------------------------
+
+		 >>> COMP WON THE GAME !
+
+
+---------------------------------------------------------------------------------------
+
 	 Enter YES to START NEW GAME or NO to QUIT :NO
+
+		 >>> ODD or EVEN GAME has been CLOSED !
